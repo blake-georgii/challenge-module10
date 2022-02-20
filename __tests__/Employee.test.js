5aafb0683cc571e119db3d43e7fe96a276a13e16
Blake Georgii <blakegeorgii@gmail.com>
2022-02-19 21:03:02 -0800

    added employee object test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,9 @@
 const Employee = require('../lib/Employee.js');
+
+test('creates an Employee object', () => {
+    const dave = new Employee('Dave', 1, 'dave@davemail.com');
+
+    expect(dave.name).toBe('Dave');
+    expect(dave.id).toBe(1);
+    expect(dave.email).toBe('dave@davemail.com');
+});
