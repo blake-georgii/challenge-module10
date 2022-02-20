7f6c6175479f327f3a3c9cd76ad73ba4837e145b
Blake Georgii <blakegeorgii@gmail.com>
2022-02-19 21:07:51 -0800

    engineer object test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,9 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an Employee object', () => {
-    const dave = new Employee('Dave', 1, 'dave@davemail.com');
+    const employee = new Employee('Dave', 1, 'dave@davemail.com');
 
-    expect(dave.name).toBe('Dave');
-    expect(dave.id).toBe(1);
-    expect(dave.email).toBe('dave@davemail.com');
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toBe(1);
+    expect(employee.email).toBe('dave@davemail.com');
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,10 @@
 const Engineer = require('../lib/Engineer.js');
 
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Dave', 1, 'dave@davemail.com', 'dave-github');
+
+    expect(engineer.name).toBe('Dave');
+    expect(engineer.id).toBe(1);
+    expect(engineer.email).toBe('dave@davemail.com');
+    expect(engineer.github).toBe('dave-github');
+});
