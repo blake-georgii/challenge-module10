5440b590e8805888db0d94cf09906a01ea554b68
Blake Georgii <blakegeorgii@gmail.com>
2022-02-19 21:10:39 -0800

    manager object test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,10 @@
 const Manager = require('../lib/Manager.js');
 
+test('creates an Manager object', () => {
+    const manager = new Manager('Dave', 1, 'dave@davemail.com', 1);
+
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toBe(1);
+    expect(manager.email).toBe('dave@davemail.com');
+    expect(manager.officeNumber).toBe(1);
+});
