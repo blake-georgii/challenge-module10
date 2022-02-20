8658b31078c4c83205884161616a7117b96f8650
Blake Georgii <blakegeorgii@gmail.com>
2022-02-19 21:09:13 -0800

    intern object test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,10 @@
 const Intern = require('../lib/Intern.js');
 
+test('creates an Intern object', () => {
+    const intern = new Intern('Dave', 1, 'dave@davemail.com', 'dave-school');
+
+    expect(intern.name).toBe('Dave');
+    expect(intern.id).toBe(1);
+    expect(intern.email).toBe('dave@davemail.com');
+    expect(engineer.school).toBe('dave-school');
+});
