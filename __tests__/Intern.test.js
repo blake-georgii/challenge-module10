667868b5d16c633a0c12bdc6df61072098d67f8f
Blake Georgii <blakegeorgii@gmail.com>
2022-02-22 22:37:12 -0800

    Intern getRole() function

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -6,7 +6,7 @@ test('creates an Intern object', () => {
     expect(intern.name).toBe('Dave');
     expect(intern.id).toBe(1);
     expect(intern.email).toBe('dave@davemail.com');
-    expect(engineer.school).toBe('dave-school');
+    expect(intern.school).toBe('dave-school');
 });
 
 test('getSchool() works correctly"', () => {
